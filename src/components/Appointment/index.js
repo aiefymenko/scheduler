1e317f4a0b6f8463d3414f7dc36417695751184a
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-05-24 17:19:40 -0400

    Request days from database

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import Header from './Header';
-import Show from './Show';
-import Empty from './Empty';
+import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "../Appointment/styles.scss";
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>}
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
     </article>
-  )
+  );
 }
