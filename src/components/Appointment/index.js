15eba2cfe5086800bf2fcc73d824713f98725ab6
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-06-03 19:15:49 -0400

    Create custom hook useApplicationData to move out all the helper functions from Application

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,6 +8,7 @@ import Form from "./Form";
 import "../Appointment/styles.scss";
 import Status from "./Status";
 import Confirm from "./Confirm";
+import Error from "./Error"
 
 
 const EMPTY = "EMPTY";
@@ -15,8 +16,10 @@ const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
-const CONFIRM= "CONFIRM";
-const EDIT= "EDIT";
+const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
 export default function Appointment(props) {
 
@@ -30,12 +33,16 @@ export default function Appointment(props) {
       interviewer
     };
     transition(SAVING);
-    props.bookInterview(props.id, interview).then(() => {transition(SHOW)}) 
+    props.bookInterview(props.id, interview)
+    .then(() => {transition(SHOW)})
+    .catch(error => {transition(ERROR_SAVE, true)})
   };
 
   function deleteInterview(name, interviewer) {
-    transition(DELETING); 
-    props.cancelInterview(props.id).then(() => {transition(EMPTY)})
+    transition(DELETING, true); 
+    props.cancelInterview(props.id)
+    .then(() => {transition(EMPTY)})
+    .catch(error => {transition(ERROR_DELETE, true)})
   }
   
   return (
@@ -77,6 +84,14 @@ export default function Appointment(props) {
   onSave={save}
   onCancel={() => back()}
   />}
+  {mode === ERROR_SAVE && <Error
+  message="Error"
+  onClose={back}
+  /> }
+    {mode === ERROR_DELETE && <Error
+  message="Error"
+  onClose={back}
+  /> }
     </article>
   );
 }
